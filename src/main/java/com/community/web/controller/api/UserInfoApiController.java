206e1028f1069a3038b20173298aa098a5373891
wubiao <wubiao@weidian.com>
2018-05-01 08:21:46 +0800

    init

--- src/main/java/com/community/web/controller/api/UserInfoApiController.java
+++ src/main/java/com/community/web/controller/api/UserInfoApiController.java
@@ -4,12 +4,13 @@
 
 package com.community.web.controller.api;
 
+import com.community.manager.UserInfoManager;
 import com.community.service.UserInfoService;
 import com.community.web.core.AbstractAction;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class UserInfoApiController extends AbstractAction<UserInfoService> {
+public class UserInfoApiController extends AbstractAction<UserInfoService, UserInfoManager> {
 
 
 }
