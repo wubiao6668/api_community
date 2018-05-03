b43f1d5eb23630f5f54fe8fc89754912daa06042
wubiao <wubiao@weidian.com>
2018-05-03 10:18:45 +0800

    initVO

---
+++ src/main/java/com/community/domain/response/vo/MessageSystemVO.java
@@ -0,0 +1,20 @@
+/**
+ * Created by wubiao on - 2018/05/03.
+ */
+
+package com.community.domain.response.vo;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class MessageSystemVO implements Serializable {
+    private static final long serialVersionUID = 3148176768559230877L;
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
